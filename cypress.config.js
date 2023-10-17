2e0e7121b5dbb8406d2870f67bf5d306ec4652fc
wooiliang <chuawooiliang@gmail.com>
2023-10-17 18:43:54 +0800

    Change the url from test env to dev env.

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://employer-test.fastjobs.my/",
+    baseUrl: "https://employer-dev.fastjobs.my/",
     experimentalSessionAndOrigin: true
   },
   env: {
